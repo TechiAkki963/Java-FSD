3a7a1947cdd8dda8419613f8ebff316f9033fb21
TechiAkki963 <paraba777@gmail.com>
2023-11-20 17:16:33 +0530

    Variable

---
+++ 02_Variable/Variable.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+
+public class Variable{
+    public static void main(String a[]){
+
+
+        byte b = 6;
+        System.out.println(b);  //**Output : 6 */
+
+        b = 25;
+        System.out.println(b); //**Output: 25 */
+
+        // b=128;
+        // System.out.println(b);
+        //!.\Variable.java:13: error: incompatible types: possible lossy conversion from int to byte
+        //! b=128;
+        //!   ^
+        //**error is because the range of Byte Datatype is -128 to 127 */
+        
+        b=127;
+        System.out.println(b); /**Output: 127 */
+    }
+}
